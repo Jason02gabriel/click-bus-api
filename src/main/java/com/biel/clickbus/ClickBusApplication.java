9c828b7dbe07a73e51914b56164eb1bcdf836844
Jason_Gabriel <j.gabrielaraujodasilva@gmail.com>
2024-05-10 09:02:12 -0300

    Initial commit

---
+++ src/main/java/com/biel/clickbus/ClickBusApplication.java
@@ -0,0 +1,13 @@
+package com.biel.clickbus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClickBusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClickBusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/biel/clickbus/ClickBusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biel.clickbus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClickBusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
